ee633f8c305885cdf335e59286055175f71b27a9
Andrew Shershunov <the.evilur@gmail.com>
2026-03-21 23:03:35 +0300

    Initial commit

---
+++ src/Main.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace Burlak {
+    [BepInPlugin(ModInfo.GUID, ModInfo.MODNAME, ModInfo.VERSION)]
+    internal class Main : BaseUnityPlugin {
+        private static ConfigEntry<string> _locale_ship;
+        private static ConfigEntry<string> _locale_pull;
+
+        public static string LocaleShip() => _locale_ship.Value;
+        public static string LocalePull() => _locale_pull.Value;
+
+        private void Awake() {
+            /* Patch all the patches */
+            Harmony harmony = new Harmony(ModInfo.GUID);
+            harmony.PatchAll();
+
+            /* Setting config entries */
+            _locale_ship = Config.Bind(
+                "Locale",
+                "Ship",
+                "Ship",
+                "Localication of the 'ship' word"
+            );
+            _locale_pull = Config.Bind(
+                "Locale",
+                "Pull",
+                "Pull",
+                "Localication of the 'pull' word"
+            );
+        }
+    }
+}
---
+++ src/ModInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Burlak {
+    internal readonly struct ModInfo {
+        public const string MODNAME = "Burlak",
+        AUTHOR = "Evilur",
+        GUID = AUTHOR + "." + MODNAME,
+        VERSION = "1.0.0",
+        DESCRIPTION = "Modification for Valheim that allows you to move your ships by bare hands";
+    }
+}
